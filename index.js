2c6861d516fe462396b1abca380f54288c6afada
Nazril <73063248+NightHawkOne@users.noreply.github.com>
2024-08-16 18:20:23 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const el = document.querySelector("#button")
+const p = document.querySelector("#p")
+el.addEventListener("click",() => alert("ok"))
+function a(){
+  alert("ini alert mu")
+  p.innerHTML = "element di ubah"
+}
+  
